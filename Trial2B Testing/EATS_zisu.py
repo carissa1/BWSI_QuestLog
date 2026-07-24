93dec4a20643dc674a36d0848177b7e2fc3fea5b
rocktvor <zisu1117@gmail.com>
2026-07-24 13:00:02 -0700

    Update EATS_zisu.py

--- Trial2B Testing/EATS_zisu.py
+++ Trial2B Testing/EATS_zisu.py
@@ -1,15 +1,20 @@
 import sys
 import math
 import csv
-# If this file is nested inside a folder in the labs folder, the relative path should be [1, ../../library] instead.
+
+# If this file is nested inside a folder in the labs folder, the relative path should
+# be [1, ../../library] instead.
 sys.path.insert(1, '../../library')
 import racecar_core
 import racecar_utils as rc_utils
+
 ########################################################################################
 # Global variables
 ########################################################################################
+
 rc = racecar_core.create_racecar()
-WINDOW = 110
+
+WINDOW = 100
 RAY_WINDOW = 20
 KP = 0.01
 MIN_VALID_DIST = 1
@@ -19,16 +24,22 @@ left_max_dist = 0
 angle = 0
 speed = 1
 ROBOT_HALF_WIDTH = 10
-SAMPLES_PER_DEGREE = 1080 / 360
 
 def start ():
     rc.drive.set_speed_angle(0, 0)
     rc.drive.set_max_speed(1)
     data = ['Speed', 'Angle', 'Error', 'lidar_left', 'lidar_right']
-   
-def is_valid(dist):
-    return dist is not None and dist > MIN_VALID_DIST
+    with open('log_wall.csv', mode='w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
     
+
+def is_valid(dist):
+    
+    return dist is not None and dist > MIN_VALID_DIST
+
+SAMPLES_PER_DEGREE = 1080 / 360
+
 def get_angle_range(scan, start_deg, end_deg):
     start_idx = int(start_deg * SAMPLES_PER_DEGREE)
     end_idx = int(end_deg * SAMPLES_PER_DEGREE)
@@ -43,6 +54,8 @@ def get_dist_angle (scan, window, window_start_deg):
             max_dist = RANGE
         return max_dist, angle_deg
     
+
+
 def update():
     global WINDOW
     scan = rc.lidar.get_samples()
@@ -56,11 +69,13 @@ def update():
     right_dist = right_max_dist - ROBOT_HALF_WIDTH / math.cos(360-right_angle)
     total_dist = right_dist + left_dist
     target_angle = (right_wt * right_dist - left_wt * left_dist)/total_dist
+    target_angle = (right_wt * right_max_dist - left_wt * left_max_dist)/total_dist
     angle = target_angle * KP
     angle = rc_utils.clamp(angle, -1, 1)
-    speed = rc_utils.remap_range(abs(angle), 0, 1, 1, 0.25, saturate=True)
+    speed = rc_utils.remap_range(abs(angle), 0, 1, 1, 0.6, saturate=True)
     print(f"{right_angle=}, {left_angle=}, {right_max_dist=}, {left_max_dist=} {target_angle=}")
     rc.drive.set_speed_angle(speed, angle)
+
 
 def update_slow():
     pass
